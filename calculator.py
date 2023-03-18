b8671f72e734dc5d4fc08803e1a5b7c715f21d7e
Victor <83128214+VictorrLim@users.noreply.github.com>
2023-03-17 23:48:12 -0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -16,14 +16,24 @@ class Calculator(QWidget):
         super().__init__()
         self.setWindowTitle("Calculator")
         self.setGeometry(100, 100, 300, 300)
-        grid = QGridLayout()
-        self.setLayout(grid)
         self.result = QLineEdit("0")
         self.result.setAlignment(Qt.AlignRight)
         self.result.setFixedHeight(50)
         self.result.setFont(QFont("Arial", 20))
         self.result.setMaxLength(8)
+
+        grid = self.create_grid_layout()
+        self.create_buttons(grid)
+
+        self.show()
+
+    def create_grid_layout(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
         grid.addWidget(self.result, 0, 0, 1, 4)
+        return grid
+
+    def create_buttons(self, grid):
         names = [
             "7",
             "8",
@@ -46,29 +56,17 @@ class Calculator(QWidget):
         for position, name in zip(positions, names):
             if name == "":
                 continue
-            button = QPushButton(name)
-            button.setFixedSize(50, 50)
-            button.setStyleSheet(
-                """
-                QPushButton {
-                    background-color: #f0f0f0;
-                    border: 1px solid #333;
-                    border-radius: 5px;
-                    font-size: 20px;
-                    font-weight: bold;
-                    color: #333;
-                }
-                QPushButton:hover {
-                    background-color: #333;
-                    color: #f0f0f0;
-                }
-                """
-            )
+            button = self.create_button(name)
             grid.addWidget(button, *position)
             button.clicked.connect(self.buttonClicked)
-        clearButton = QPushButton("C")
-        clearButton.setFixedSize(50, 50)
-        clearButton.setStyleSheet(
+        clearButton = self.create_button("C")
+        grid.addWidget(clearButton, 4, 3)
+        clearButton.clicked.connect(self.clearClicked)
+
+    def create_button(self, name):
+        button = QPushButton(name)
+        button.setFixedSize(50, 50)
+        button.setStyleSheet(
             """
             QPushButton {
                 background-color: #f0f0f0;
@@ -84,9 +82,7 @@ class Calculator(QWidget):
             }
             """
         )
-        grid.addWidget(clearButton, 4, 3)
-        clearButton.clicked.connect(self.clearClicked)
-        self.show()
+        return button
 
     def buttonClicked(self):
         button = self.sender()
@@ -95,7 +91,7 @@ class Calculator(QWidget):
             result = str(eval(self.result.text()))
             self.result.setText(result)
         elif key == "C":
-            self.result.setText("0")
+            self.clearClicked()
         else:
             if self.result.text() == "0":
                 self.result.setText(key)
@@ -109,20 +105,20 @@ class Calculator(QWidget):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
-    palette = QPalette()
-    palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    palette.setColor(QPalette.WindowText, Qt.white)
-    palette.setColor(QPalette.Base, QColor(25, 25, 25))
-    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    palette.setColor(QPalette.ToolTipBase, Qt.white)
-    palette.setColor(QPalette.ToolTipText, Qt.white)
-    palette.setColor(QPalette.Text, Qt.white)
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.ButtonText, Qt.white)
-    palette.setColor(QPalette.BrightText, Qt.red)
-    palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-    palette.setColor(QPalette.HighlightedText, Qt.black)
-    app.setPalette(palette)
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.setPalette(dark_palette)
     calc = Calculator()
     sys.exit(app.exec_())
